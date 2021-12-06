4f31fbab097a9ca3724124d6bfc621666b97c346
Luis Antonio Sánchez Monterde <antonio.dgr.ink@gmail.com>
2021-12-06 16:48:00 -0600

    Terminado dia 4, buenas sensaciones con go

--- day4/day4.go
+++ day4/day4.go
@@ -7,14 +7,19 @@ import "strings"
 import "strconv"
 
 func main() {
-	//throws = getThrows()
+	throws := getThrows("input")
+	boards := getBoards("input")
+	fmt.Printf("Puntuación 1: %d\n", computeGame(throws, boards))
+	throws2 := getThrows("input")
+	boards2 := getBoards("input")
+	fmt.Printf("Puntuación 2: %d\n", computeGame2(throws2, boards2))
 }
 
 type Board struct{
 }
 
-func getData()([]int, []Board) {
-	file, _ := os.OpenFile("input-test", os.O_RDONLY, 0644)
+func getThrows(data_input string)([]int) {
+	file, _ := os.OpenFile(data_input, os.O_RDONLY, 0644)
 	defer file.Close()
 
 	line := bufio.NewScanner(file)
@@ -24,7 +29,7 @@ func getData()([]int, []Board) {
 	throws_string := strings.Split(throws_array, ",")
 	throws, _ := sliceAtoi(throws_string)
 
-	return throws, []Board{}
+	return throws
 }
 
 func sliceAtoi(sa []string) ([]int, error) {
@@ -39,11 +44,10 @@ func sliceAtoi(sa []string) ([]int, error) {
     return si, nil
 }
 
-func getBoard() [][]int{
-	file, _ := os.OpenFile("input-test", os.O_RDONLY, 0644)
+func getBoards(data_input string) [][]int{
+	file, _ := os.OpenFile(data_input, os.O_RDONLY, 0644)
 	defer file.Close()
 	line := bufio.NewScanner(file)
-	line.Scan()
 	line.Scan()
 	board := [][]int{{}}
 
@@ -64,5 +68,125 @@ func getBoard() [][]int{
 			i++
 		}
 	}
-	return board
+	return board[:len(board) - 1]
+}
+
+func throwMarkBoard(throw int, board []int){
+	for i, _ := range board{
+		if(board[i] == throw){
+			board[i] = -1
+		}
+	}
+}
+
+func throw(throw int, boards [][]int){
+	for i, _ := range boards{
+		throwMarkBoard(throw, boards[i])
+	}
+}
+
+func completedRow(board []int, row int) bool{
+	columns := []int{}
+	if row == 1{
+		columns = append(columns, []int{0, 1, 2, 3, 4}...)
+	}else if row == 2{
+		columns = append(columns, []int{5, 6, 7, 8, 9}...)
+	}else if row == 3{
+		columns = append(columns, []int{10, 11, 12, 13, 14}...)
+	}else if row == 4{
+		columns = append(columns, []int{15, 16, 17, 18, 19}...)
+	}else if row == 5{
+		columns = append(columns, []int{20, 21, 22, 23, 24}...)
+	}else{
+		columns = append(columns, []int{}...)
+	}
+	for _, i := range columns{
+		if board[i] != -1{
+			return false
+		}
+	}
+	return true
+}
+
+func completedColumn(board []int, column int) bool{
+	rows := []int{}
+	if column == 1{
+		rows = append(rows, []int{0, 5, 10, 15, 20}...)
+	}else if column == 2{
+		rows = append(rows, []int{1, 6, 11, 16, 21}...)
+	}else if column == 3{
+		rows = append(rows, []int{2, 7, 12, 17, 22}...)
+	}else if column == 4{
+		rows = append(rows, []int{3, 8, 13, 18, 23}...)
+	}else if column == 5{
+		rows = append(rows, []int{4, 9, 14, 19, 24}...)
+	}else{
+		rows = append(rows, []int{}...)
+	}
+	for _, i := range rows{
+		if board[i] != -1{
+			return false
+		}
+	}
+	return true
+}
+
+func completedBoard(board []int) bool{
+	for i := 1; i<6; i++{
+		if completedRow(board, i) || completedColumn(board, i){
+			return true
+		}
+	}
+	return false
+}
+
+func completedAnyBoard(board [][]int) (bool, int){
+	for i, _ := range board{
+		if completedBoard(board[i]){
+			return true, i
+		}
+	}
+	return false, -1
+}
+
+func computeWinnerBoard(board []int) int{
+	sumator := 0
+	for _, value := range board{
+		if value != -1{
+			sumator += value
+		}
+	}
+	return sumator
+}
+
+func computeGame(throws []int, boards [][]int) int{
+	for _, value := range throws{
+		throw(value, boards)
+		winner, boardWinner := completedAnyBoard(boards)
+		if winner{
+			return value * computeWinnerBoard(boards[boardWinner])
+		}
+	}
+	return 0
+}
+
+func computeGame2(throws []int, boards [][]int) int{
+	i := 0
+	for ; i<(len(throws)-1); i++{
+		throw(throws[i], boards)
+		completed, number_completed := completedAnyBoard(boards)
+		for completed{
+			if len(boards) == 1 && completedBoard(boards[0]){
+				return throws[i] * computeWinnerBoard(boards[0])
+			}
+			if completed{
+				boards = append(boards[:number_completed], 
+					boards[number_completed+1:]...)
+			}
+			completed, number_completed = completedAnyBoard(boards)
+		}
+	}
+	fmt.Println(boards[0])
+	fmt.Println(len(boards))
+	return throws[i] * computeWinnerBoard(boards[0])
 }
--- day4/day4_test.go
+++ day4/day4_test.go
@@ -3,18 +3,9 @@ package main
 import "testing"
 import "reflect"
 
-
-func TestThrows(t *testing.T){
-	got, _ := getData()
-	want := []int {7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1}
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %q want %q", got, want)
-	}
-}
-
 func TestGetBoard(t *testing.T){
 	t.Run("Get first board", func(t *testing.T) {
-		got := getBoard()[0]
+		got := getBoards("input-test")[0]
 		want := []int {	22, 13, 17, 11,  0,
 		8,  2, 23,  4, 24,
 		21,  9, 14, 16,  7,
@@ -27,7 +18,7 @@ func TestGetBoard(t *testing.T){
 	})
 
 	t.Run("Get second board", func(t *testing.T) {
-		got := getBoard()[1]
+		got := getBoards("input-test")[1]
 		want := []int {	
 			3, 15,  0,  2, 22,
 			9, 18, 13, 17,  5,
@@ -37,6 +28,173 @@ func TestGetBoard(t *testing.T){
 
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %q want %q", got, want)
+		}
+	})
+}
+
+func TestThrows(t *testing.T){
+	t.Run("get throws list", func(t *testing.T) {
+		got := getThrows("input-test")
+		want := []int {	7, 4, 9, 5, 11, 17, 23, 2, 0, 14,
+				21, 24, 10, 16, 13, 6, 15, 25, 12,
+				22, 18, 20, 8, 19, 3, 26, 1}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %q want %q", got, want)	
+		}
+	})
+
+	t.Run("Mark 2 in second board, but no mark 3 in first board",
+			func(t *testing.T) {
+		board := getBoards("input-test")[1]
+		throwMarkBoard(2, board)
+		got2 := board[3]
+		got3 := board[0]
+		want2 := -1
+		want3 := 3
+
+		if got2 != want2 {
+			t.Errorf("got %q want %q", got2, want2)
+		}
+		if got3 != want3 {
+			t.Errorf("got %q want %q", got3, want3)
+		}
+	})
+
+	t.Run("Mark 2 in all boards ", func(t *testing.T) {
+		boards := getBoards("input-test")
+		throw(0, boards)
+		got := boards[0][4]
+		got2 := boards[1][2]
+		want := -1
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+		if got2 != want {
+			t.Errorf("got %q want %q", got2, want)
+		}
+	})
+}
+
+
+func TestCompletedLine(t *testing.T){
+	t.Run("Check completed first row in first board", func(t *testing.T) {
+		boards := getBoards("input-test")
+		throw(22, boards)
+		throw(13, boards)
+		throw(17, boards)
+		throw(11, boards)
+		throw(0, boards)
+		got := completedRow(boards[0], 1)
+		want := true
+
+		if got != want {
+			t.Errorf("got %t want %t", got, want)
+		}
+	})
+
+	t.Run("Check completed second row in second board", func(t *testing.T) {
+		boards := getBoards("input-test")
+		throw(9, boards)
+		throw(18, boards)
+		throw(13, boards)
+		throw(17, boards)
+		throw(5, boards)
+		got := completedRow(boards[1], 2)
+		want := true
+
+		if got != want {
+			t.Errorf("got %t want %t", got, want)
+		}
+	})
+
+	t.Run("Check completed first column in first board", func(t *testing.T) {
+		boards := getBoards("input-test")
+		throw(22, boards)
+		throw(8, boards)
+		throw(21, boards)
+		throw(6, boards)
+		throw(1, boards)
+		got := completedColumn(boards[0], 1)
+		want := true
+
+		if got != want {
+			t.Errorf("got %t want %t", got, want)
+		}
+	})
+}
+
+func TestCompletedBoard(t *testing.T){
+	t.Run("Check completed board", func(t *testing.T) {
+		boards := getBoards("input-test")
+		throw(22, boards)
+		throw(8, boards)
+		throw(21, boards)
+		throw(6, boards)
+		throw(1, boards)
+		got := completedBoard(boards[0])
+		want := true
+
+		if got != want {
+			t.Errorf("got %t want %t", got, want)
+		}
+	})
+}
+
+func TestCompletedAnyBoard(t *testing.T){
+	t.Run("Check completed Any board", func(t *testing.T) {
+		boards := getBoards("input-test")
+		throw(4, boards)
+		throw(19, boards)
+		throw(20, boards)
+		throw(5, boards)
+		throw(7, boards)
+		got, _ := completedAnyBoard(boards)
+		want := true
+
+		if got != want {
+			t.Errorf("got %t want %t", got, want)
+		}
+	})
+}
+
+func TestComputeResult(t *testing.T){
+	t.Run("Check computed result ", func(t *testing.T) {
+		boards := getBoards("input-test")
+		throw(4, boards)
+		throw(19, boards)
+		throw(20, boards)
+		throw(5, boards)
+		throw(7, boards)
+		got := computeWinnerBoard(boards[2])
+		want := 270
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("Check game result", func(t *testing.T) {
+		throws := getThrows("input-test")
+		boards := getBoards("input-test")
+		got := computeGame(throws, boards)
+		want := 4512
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+}
+
+func TestComputeResult2part(t *testing.T){
+	t.Run("Check game result", func(t *testing.T) {
+		throws := getThrows("input-test")
+		boards := getBoards("input-test")
+		got := computeGame2(throws, boards)
+		want := 1924
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
 		}
 	})
 }
